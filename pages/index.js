a29f4011ddd4c78c2f59e9fe7b00cf47fcf98161
cdn64 <gitcdn@gmail.com>
2018-05-27 22:12:27 +0200

    Logo added

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import React, {Component} from "react"
 import {ParallaxProvider} from "react-scroll-parallax"
 
 import {Venue, Speakers} from "../segments"
-import {Separator, Separator2} from "../presentational"
+import {Separator, Separator2, Logo} from "../presentational"
 
 export default class extends Component {
   render() {
     return (
       <ParallaxProvider>
         <React.Fragment>
+          <Logo />
           {[
             Separator,
             Venue,
@@ -20,7 +21,9 @@ export default class extends Component {
             Speakers,
             Speakers,
             Venue
-          ].map(Component => <Component url={this.props.url} />)}
+          ].map((Component, index) => (
+            <Component key={index} url={this.props.url} />
+          ))}
         </React.Fragment>
       </ParallaxProvider>
     )
---
+++ presentational/Logo.js
@@ -0,0 +1,34 @@
+import React from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  display: inline-block;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 500;
+
+  padding: 28px 12px;
+  margin-top: -12px;
+  background-color: #fff;
+  border-left: 3px solid black;
+  border-bottom: 3px solid black;
+  transform: skew(0deg, 6deg);
+`
+
+const Img = styled.img`
+  width: 200px;
+  transform: skew(0deg, -6deg);
+`
+
+export class Logo extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Wrapper>
+          <Img src="/static/TEDxBern.svg" />
+        </Wrapper>
+      </React.Fragment>
+    )
+  }
+}
